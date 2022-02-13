36054b4b381b8430addb790a1a7f193cbe1e63bc
Ale Rayz <rayz1065@gmail.com>
2022-02-13 21:56:41 +0100

    Added filter by year for student selection Fixed bug on path picker Removed check mark from the section options, improved section selection

--- lib/app/state/settings_state.dart
+++ lib/app/state/settings_state.dart
@@ -10,6 +10,7 @@ class SettingsState extends State<SettingsPage> {
 
   String section = "";
   String path = "";
+  int? selectedClass;
 
   @override
   void initState() {
@@ -23,19 +24,14 @@ class SettingsState extends State<SettingsPage> {
   }
 
   void setSection(String section) {
-    setState(() {
-      this.section = section;
-    });
-  }
-
-  Widget getTick(bool condition, Color color) {
-    return Icon(
-      condition
-          ? CupertinoIcons.check_mark_circled_solid
-          : CupertinoIcons.check_mark_circled,
-      color: color,
-      size: 35,
-    );
+    if (section != this.section) {
+      setState(() {
+        this.section = section;
+        // reset the path since it's not longer valid
+        path = "";
+        selectedClass = null;
+      });
+    }
   }
 
   void confirm(BuildContext context) async {
@@ -55,6 +51,14 @@ class SettingsState extends State<SettingsPage> {
       return;
     }
 
+    var options = data[section];
+    if (section == 'CLASSI' && selectedClass != null) {
+      // filter the options by class
+      options = options
+          .where((x) => x.startsWith(selectedClass.toString()) ? true : false)
+          .toList();
+    }
+
     showCupertinoModalPopup(
         context: context,
         builder: (_) => SizedBox(
@@ -62,15 +66,17 @@ class SettingsState extends State<SettingsPage> {
             child: CupertinoPicker(
               itemExtent: 30,
               onSelectedItemChanged: (index) {
-                setState(() {
-                  path = data[section][index - 1];
-                });
+                if (index > 0) {
+                  setState(() {
+                    path = options[index - 1];
+                  });
+                }
               },
               backgroundColor: CupertinoColors.systemBackground,
               children: [
                 const Text("Seleziona un orario",
                     style: TextStyle(fontSize: 20)),
-                for (var item in data[section])
+                for (var item in options)
                   Text(item, style: const TextStyle(fontSize: 20)),
               ],
             )));
@@ -89,13 +95,39 @@ class SettingsState extends State<SettingsPage> {
             children: [
               Icon(icon, size: 100, color: color),
               Text(name, style: TextStyle(fontSize: 25, color: color)),
-              getTick(section == code, color),
             ],
           ),
         ),
         onTap: () {
           setSection(code);
         });
+  }
+
+  Widget classFilter() {
+    return Column(children: [
+      const Text("Filtra per anno"),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (var classNum in [1, 2, 3, 4, 5])
+            CupertinoButton(
+                child: Text(classNum.toString(),
+                    style: TextStyle(
+                        color:
+                            selectedClass == classNum ? null : getTextColor(),
+                        fontSize: 25,
+                        fontWeight: selectedClass == classNum
+                            ? FontWeight.bold
+                            : FontWeight.normal)),
+                onPressed: () {
+                  if (selectedClass != classNum) {
+                    setState(() => {selectedClass = classNum, path = ""});
+                  }
+                  showPathDialog();
+                })
+        ],
+      )
+    ]);
   }
 
   Widget createPage() {
@@ -121,6 +153,12 @@ class SettingsState extends State<SettingsPage> {
         ],
       ),
       const Padding(padding: EdgeInsets.all(25)),
+      SizedBox(
+        height: 70,
+        child: section == 'CLASSI'
+            ? classFilter()
+            : const Padding(padding: EdgeInsets.all(35)),
+      ),
       GestureDetector(
         child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(path == '' ? "Tap per selezionare" : path,
@@ -131,7 +169,7 @@ class SettingsState extends State<SettingsPage> {
           showPathDialog();
         },
       ),
-      Padding(padding: EdgeInsets.all(35)),
+      const Padding(padding: EdgeInsets.all(35)),
       CupertinoButton.filled(
         child: const Text("CONFERMA", style: TextStyle(fontSize: 25)),
         onPressed: () {
